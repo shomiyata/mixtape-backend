2b2ddff70715a15d648343763e6a1cfdf4ee3f63
shomiyata <eighteighttwenty@gmail.com>
2017-09-28 17:44:43 -0400

    Add to spotify button now working

--- app/controllers/api/v1/mixtapes_controller.rb
+++ app/controllers/api/v1/mixtapes_controller.rb
@@ -65,25 +65,35 @@ class Api::V1::MixtapesController < ApplicationController
   end
 
   def build
-      header = { Authorization: "Bearer #{current_user.access_token}" }
-      api_url = "https://api.spotify.com/v1/users/#{current_user.spotify_user_id}/playlists"
-      user_response = RestClient.post(api_url, header)
-      new_playlist = JSON.parse(user_response.body)
-      puts new_playlist
-        # mixtape = Mixtape.find_by(spotify_playlist_id: params[:id])
-        # tracks = mixtape.tracks
-        #
-        # header = {
-        #   Authorization: "Bearer #{current_user.access_token}",
-        #   content_type: "application/json" }
-        #
-        # body = {
-        #   "uris": tracks
-        # }
-        # user_response = RestClient.post("https://api.spotify.com/v1/users/#{current_user.spotify_user_id}/playlists/#{params[:id]}/tracks", body.to_json, header.to_json)
-        # response = JSON.parse(user_response.body)
-        # render json: response, status: 200
+    puts params
+    mixtape = Mixtape.find_by(spotify_playlist_id: params[:mixtapeId])
+    tracks = mixtape.tracks
 
+    track_uris = tracks.map do |track|
+      track[:track_uri]
+    end
+
+    header = {
+      "Authorization" => "Bearer #{current_user.access_token}" ,
+      "Content-Type" => "application/json"
+    }
+
+    body = {
+      name: params[:playlistName]
+    }
+
+    api_url = "https://api.spotify.com/v1/users/#{current_user.spotify_user_id}/playlists"
+    api_response = RestClient.post(api_url, body.to_json, header)
+    new_playlist = JSON.parse(api_response.body)
+
+    track_body = {
+      uris: track_uris
+    }
+
+    tracks_api_url = "https://api.spotify.com/v1/users/#{current_user.spotify_user_id}/playlists/#{new_playlist["id"]}/tracks"
+    tracks_api_response = RestClient.post(tracks_api_url, track_body.to_json, header)
+    parsed_tracks_api_response = JSON.parse(tracks_api_response.body)
+    render json: parsed_tracks_api_response, status: 200
   end
 
   def show
